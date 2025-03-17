0d9e9d6a3f94f056890176b97cfd8f887c435591
Dilraj Singh <dilraj082@gmail.com>
2025-03-17 12:36:45 +0530

    removed user file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.api.routes import users, emails
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.routes import emails
 from app.core.config import settings
 from app.db.session import engine, Base
 
@@ -9,8 +10,16 @@ Base.metadata.create_all(bind=engine)
 # Initialize FastAPI app
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update this to specific domains if needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Include routers
-app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(emails.router, prefix="/emails", tags=["Emails"])
 
 @app.get("/")
